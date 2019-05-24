e9cde109f3fc8563bd66dc85f3542be3a3f6f0f6
julio lemus <juliolemus@Mark-50.local>
2019-05-23 20:24:50 -0700

    Updated to check mutations size instead of looping

--- content.js
+++ content.js
@@ -2,11 +2,11 @@ window.addEventListener('load', function(){
 	const config = {childList: true}; // errors if we don't provide at least one option
 	const ytVideoAdContainer = document.querySelector(".video-ads.ytp-ad-module");
 	const observer = new MutationObserver(function(mutations){
-		mutations.forEach(function(mutation){
+		if( mutations.length > 0){
 			if ( document.querySelector(".ytp-ad-skip-button") ){
 				document.querySelector(".ytp-ad-skip-button").click();
 			}
-		});
+		}
 	});
 
 	// if main ads container found, initiate observer else disconnect
