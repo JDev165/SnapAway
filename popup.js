2690eddf7ac6ca97a478d0c6b7cbe2d2cf1b30d6
julio lemus <juliolemus@Mark-50.local>
2019-05-23 20:05:16 -0700

    Removed popup.js content

--- popup.js
+++ popup.js
@@ -1,8 +1,1 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-let toggle = document.getElementById("toggle-status");
-let skip = document.getElementsByClassName("ytp-ad-skip-button");
+// add supporting js for popup.html
